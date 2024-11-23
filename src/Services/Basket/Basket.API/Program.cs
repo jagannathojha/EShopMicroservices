01948ecb525add6f290150ebd5b781d27f525fa5
Jagannath Ojha <ojha.jagann@gmail.com>
2024-11-23 13:36:59 +0545

    update orchastrate basket service with grpc

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -34,6 +34,13 @@ builder.Services.AddStackExchangeRedisCache(opts =>
 builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
 {
     options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+}).ConfigurePrimaryHttpMessageHandler(() =>
+{
+    var handler = new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator,
+    };
+    return handler;
 });
 
 //Cross-Cutting Services
