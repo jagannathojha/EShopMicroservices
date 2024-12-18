7bf29a5f0a3a2297bbccbcb19817753585f422c7
Jagannath Ojha <ojha.jagann@gmail.com>
2024-12-18 22:07:15 +0545

    order queries

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace Ordering.Application.Orders.EventHandlers.Domain;
+public class OrderCreatedEventHandler
+    (ILogger<OrderCreatedEventHandler> logger)
+    : INotificationHandler<OrderCreatedEvent>
+{
+    public Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
+
+        //if (await featureManager.IsEnabledAsync("OrderFullfilment"))
+        //{
+        //    var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
+        //    await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
+        //}
+        return Task.CompletedTask;
+    }
+}
